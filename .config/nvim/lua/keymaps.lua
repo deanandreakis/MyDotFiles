4c3bb200fc759bfc83fa8f860c00feb4ed2bb149
Dean Andreakis <dean.andreakis@walmart.com>
2022-07-19 15:36:15 -0700

    add grep

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,4 +1,5 @@
 vim.api.nvim_set_keymap("n", "<C-o>", ":NvimTreeToggle<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-p>", ":Telescope find_files<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-r>", ":Telescope live_grep<CR>", { noremap = true })
 vim.g.mapleader = ";"
 vim.g.maplocalleader = ","
