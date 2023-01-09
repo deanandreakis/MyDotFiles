5ff22f9b4c032ed2e76cd0b60e756133f15d7936
Dean Andreakis <dean@deanware.com>
2023-01-09 14:14:52 -0700

    updates

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -87,3 +87,11 @@ vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
   vim.lsp.handlers.signature_help,
   {border = 'rounded'}
 )
+
+if os.getenv("ZSH_OS") == "nixos" then
+require("mason").setup({
+  install_root_dir = "/home/dean/mason",
+  PATH = "skip",
+  }
+)
+end
