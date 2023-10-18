a90af3bd7d96762839d3d4242e38d7fc600327d2
Dean Andreakis <dean@deanware.com>
2023-10-18 10:52:09 -0700

    updates

--- .config/nvim/lua/deanware/lazy.lua
+++ .config/nvim/lua/deanware/lazy.lua
@@ -32,7 +32,7 @@ require('lazy').setup({
 
   {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons', opt = true }
+    dependencies = { 'nvim-tree/nvim-web-devicons'}
   },
 
   {
