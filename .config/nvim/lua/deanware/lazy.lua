9f484ddb72347f7f12dc5c279d7060d81e98edcf
Dean Andreakis <dean@deanware.com>
2025-07-28 08:33:08 -0700

    updates

--- .config/nvim/lua/deanware/lazy.lua
+++ .config/nvim/lua/deanware/lazy.lua
@@ -72,26 +72,34 @@ require('lazy').setup({
 
   -- Plugins for LSP
   {
-	  'VonHeikemen/lsp-zero.nvim',
-	  dependencies = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
-
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lua'},
-
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},
-		  {'rafamadriz/friendly-snippets'},
-	  }
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
+    },
   },
+  -- {
+	  -- 'VonHeikemen/lsp-zero.nvim',
+	  -- dependencies = {
+		  -- -- LSP Support
+		  -- {'neovim/nvim-lspconfig'},
+		  -- {'williamboman/mason.nvim'},
+		  -- {'williamboman/mason-lspconfig.nvim'},
+
+		  -- -- Autocompletion
+		  -- {'hrsh7th/nvim-cmp'},
+		  -- {'hrsh7th/cmp-buffer'},
+		  -- {'hrsh7th/cmp-path'},
+		  -- {'saadparwaiz1/cmp_luasnip'},
+		  -- {'hrsh7th/cmp-nvim-lsp'},
+		  -- {'hrsh7th/cmp-nvim-lua'},
+
+		  -- -- Snippets
+		  -- {'L3MON4D3/LuaSnip'},
+		  -- {'rafamadriz/friendly-snippets'},
+	  -- }
+  -- },
 
   -- Plugin for vim-rooter
   'airblade/vim-rooter',
